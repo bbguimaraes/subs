e3ae4a10d9eb2a6771a8defa867967462ff5b9af
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-09-27 18:58:26 +0000

    Add terminal user interface

---
+++ src/curses/lua.c
@@ -0,0 +1,122 @@
+#include "lua.h"
+
+#include <string.h>
+
+#include <lauxlib.h>
+
+#include "../log.h"
+#include "../subs.h"
+
+#include "curses.h"
+#include "subs.h"
+#include "source.h"
+#include "videos.h"
+
+static int shell_mode_new(lua_State *L) {
+    lua_newtable(L);
+    luaL_getmetatable(L, "shell_mode");
+    lua_setmetatable(L, -2);
+    suspend_tui();
+    return 1;
+}
+
+static int shell_mode_close(lua_State *L) {
+    (void)L;
+    resume_tui();
+    return 0;
+}
+
+static int cur_item(lua_State *L) {
+    struct videos *const v = *(struct videos**)lua_touserdata(L, 1);
+    lua_pushinteger(L, v->items[v->list.i]);
+    return 1;
+}
+
+static int items_iter(lua_State *L) {
+    struct videos *const v = *(struct videos**)lua_touserdata(L, 1);
+    const lua_Integer i = lua_tointeger(L, 2);
+    if(i < 0 || v->n <= i)
+        return 0;
+    lua_pushinteger(L, i + 1);
+    lua_pushinteger(L, v->items[i]);
+    return 2;
+}
+
+static int items(lua_State *L) {
+    lua_pushcfunction(L, items_iter);
+    lua_pushvalue(L, 1);
+    lua_pushinteger(L, 0);
+    return 3;
+}
+
+static void init_videos(lua_State *L, struct videos *videos) {
+    void *const ud = lua_newuserdatauv(L, sizeof(videos), 0);
+    memcpy(ud, &videos, sizeof(videos));
+    lua_newtable(L);
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    lua_pushcfunction(L, cur_item);
+    lua_setfield(L, -2, "cur_item");
+    lua_pushcfunction(L, items);
+    lua_setfield(L, -2, "items");
+    lua_setmetatable(L, -2);
+}
+
+static void init_shell_mode(lua_State *L) {
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    lua_pushcfunction(L, shell_mode_new);
+    lua_setfield(L, -2, "new");
+    lua_pushcfunction(L, shell_mode_close);
+    lua_setfield(L, -2, "__close");
+}
+
+void init_lua(lua_State *L, struct videos *videos) {
+    lua_newtable(L);
+    lua_pushinteger(L, KEY_ERROR);
+    lua_setfield(L, -2, "KEY_ERROR");
+    lua_pushinteger(L, KEY_HANDLED);
+    lua_setfield(L, -2, "KEY_HANDLED");
+    lua_pushinteger(L, KEY_IGNORED);
+    lua_setfield(L, -2, "KEY_IGNORED");
+    init_videos(L, videos);
+    lua_setfield(L, -2, "videos");
+    luaL_newmetatable(L, "shell_mode");
+    init_shell_mode(L);
+    lua_setfield(L, -2, "shell_mode");
+    lua_setglobal(L, "curses");
+}
+
+bool calc_pos_lua(
+    lua_State *L, struct source_bar *source_bar, struct subs_bar *subs_bar,
+    struct videos *videos)
+{
+    const int top = lua_gettop(L);
+    lua_pushcfunction(L, subs_lua_msgh);
+    bool ret = false;
+    if(lua_getglobal(L, "calc_pos") == LUA_TNIL) {
+        LOG_ERR("'calc_pos' function not found\n", 0);
+        goto end;
+    }
+    lua_pushinteger(L, LINES);
+    lua_pushinteger(L, COLS);
+    lua_pushinteger(L, source_bar->n_tags);
+    if(lua_pcall(L, 3, 12, top + 1) != LUA_OK)
+        goto end;
+    source_bar->x = (int)lua_tointeger(L, 2);
+    source_bar->y = (int)lua_tointeger(L, 3);
+    source_bar->width = (int)lua_tointeger(L, 4);
+    source_bar->height = (int)lua_tointeger(L, 5);
+    subs_bar->x = (int)lua_tointeger(L, 6);
+    subs_bar->y = (int)lua_tointeger(L, 7);
+    subs_bar->width = (int)lua_tointeger(L, 8);
+    subs_bar->height = (int)lua_tointeger(L, 9);
+    videos->x = (int)lua_tointeger(L,  10);
+    videos->y = (int)lua_tointeger(L,  11);
+    videos->width = (int)lua_tointeger(L, 12);
+    videos->height = (int)lua_tointeger(L, 13);
+    ret = true;
+end:
+    lua_settop(L, top);
+    return ret;
+}
---
+++ src/curses/subs.h
@@ -0,0 +1,35 @@
+#ifndef SUBS_CURSES_SUBS_H
+#define SUBS_CURSES_SUBS_H
+
+#include <ncurses.h>
+
+#include "../def.h"
+
+#include "const.h"
+#include "list.h"
+
+struct subs_curses;
+struct window;
+
+struct subs_bar {
+    struct subs_curses *s;
+    struct videos *videos;
+    struct list list;
+    int *items;
+    int x, y, width, height, tag, type;
+    u8 flags;
+};
+
+void subs_bar_toggle_watched(struct subs_bar *b);
+void subs_bar_toggle_not_watched(struct subs_bar *b);
+void subs_bar_set_untagged(struct subs_bar *b);
+void subs_bar_set_tag(struct subs_bar *b, int t);
+void subs_bar_set_type(struct subs_bar *b, int t);
+bool subs_bar_reload(struct subs_bar *b);
+void subs_bar_destroy(struct subs_bar *b);
+bool subs_bar_leave(struct window *w);
+bool subs_bar_enter(struct window *w);
+void subs_bar_redraw(struct window *w);
+enum subs_curses_key subs_bar_input(struct window *w, int c);
+
+#endif
